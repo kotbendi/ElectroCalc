e543d3a0d045e7a9c61c2c2ce44ac9b265489f00
kotbendi <bendihaker@gmail.com>
2025-12-05 06:35:29 +0000

    Header.h

---
+++ Header.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdio.h>
+    int input;
+    int input1;
+    float a;
+    float v;
+    float om;
+    float length = 0.0;
+float calcFrequencyMHz(float lengthMeters);
+float volt(float a ,float o){
+    return a * o;
+}
+float Resistance(float v,float a){
+    return v / a;
+}
+float Hz(float leg){
+    float freqMHz = calcFrequencyMHz(leg);
+    float freqHz = freqMHz * 1000000;
+    printf("Frequency:\n");
+    printf("  %.3f MHz\n", freqMHz);
+    printf("  %.0f Hz\n", freqHz);
+    return 0;
+}
+float calcFrequencyMHz(float lengthMeters) {
+    return 150.0 / lengthMeters;
+}
