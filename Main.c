8b1b36f243dab42ca327645c9cd8054f9f21ed63
kotbendi <bendihaker@gmail.com>
2025-12-05 06:48:47 +0000

    Main.c

--- Main.c
+++ Main.c
@@ -3,13 +3,6 @@
 #include "Header.h"
 
 int main() {
-    int input;
-    int input1;
-    float a;
-    float v;
-    float om;
-    float length = 0.0;
-
     printf("Enter what you wanna 1.calc Hz 2.calc Ω: ");
     scanf("%d",&input);
     system("clear");
@@ -23,23 +16,28 @@ int main() {
         Hz(length);
     }
     else if (input == 2){
-        printf("Enter what you wanna 1.Voltage 2.Resistance \n");
+        printf("Enter what you wanna 1.Voltage 2.Resistance 3.Ampers \n");
         scanf("%d",&input1);
         if (input1 == 1){
             printf("Write Amperes: ");
             scanf("%f",&a);
             printf("Write Ω: ");
             scanf("%f",&om);
-            printf("Your voltage: ");
-            printf("%f",volt(a,om));
+            printf("%f""Your voltage: ",volt(a,om));
         }
         else if (input1 == 2){
            printf("Write volt: ");
            scanf("%f",&v);
            printf("Write Amperes: ");
            scanf("%f",&a);
-           printf("Your resistance: ");
-           printf("%f",Resistance(v,a));
+           printf("%f""Your resistance: ",Resistance(v,a));
+        }
+        else if (input1 == 3){
+            printf("write voltage: ");
+            scanf("%f",&v);
+            printf("Write Ω: ");
+            scanf("%f",&om);
+            printf("%f""You ampers is: ",Ampers(v,om));
         }
         else{
             printf("Error");
